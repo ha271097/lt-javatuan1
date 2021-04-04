3d0a98dffc250ed22e1a580a4672c16893ffb9ed
ha271097 <nguyenquangha2710@gmail.com>
2021-04-04 13:16:01 +0700

    bt5

--- src/com/company/bt5.java
+++ src/com/company/bt5.java
@@ -1,7 +1,25 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class bt5 {
     public static void main(String[]args){
-
+        System.out.println("Enter a,b,c");
+        double a,b,c;
+        Scanner sc =new Scanner(System.in);
+        a= sc.nextDouble();
+        b= sc.nextDouble();
+        c= sc.nextDouble();
+        double delta = (b*b)-4*a*c;
+        if(delta>0){
+            System.out.println(" x1 = " + (-b+Math.sqrt(delta))/2*a);
+            System.out.println("x2 = " + (-b-Math.sqrt(delta))/2*a);
+        }
+        else if(delta==0){
+            System.out.println("x =" + -b/2*a);
+        }
+        else {
+            System.out.println("PT vo nghiem");
+        }
     }
 }
